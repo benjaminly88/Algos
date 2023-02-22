b2ad5bd3e5faa6b71cc54a7cc92b0ba2c9ad9b62
Benjamin Ly <benjaminly702@gmail.com>
2023-02-22 15:22:01 -0800

    Completed apple-stocks

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -20,22 +20,34 @@ calculate profit along the array.
 
 */
 
-function highestProfit(apple_stock, output = 0) {
+const { isFunctionTypeNode } = require('typescript');
+
+function highestProfit(apple_stock) {
+  let output = 0;
   if (!apple_stock) {
     return output;
   }
-
-  // }
+  let lowestVal = Infinity;
+  for (let i = 0; i < apple_stock.length; i++) {
+    if (apple_stock[i] < lowestVal) {
+      lowestVal = apple_stock[i];
+    } else if (apple_stock[i] - lowestVal > output) {
+      output = apple_stock[i] - lowestVal;
+    }
+  }
+  return output;
 }
 
 const stock = [0, 300, 200, 500, 600, 100, 399, 350, 500];
 console.log(highestProfit(stock));
 
+// create a lowest value = Infinite
 // iterate through the array
-// if the next value in the array is greater than the current value
-// add it onto the return value
-// keep track of the lowest values and the highest values
-//
-// return 0 if input is invalid
+// if the current element is less tahn the lowest value,
+// reassign the lowest value to the element
+// if the current element is greater than the lowest value
+// check if the current element - lowest value is greater than the output
+// if so reassign output
+// return output
 
 module.exports = { highestProfit };
